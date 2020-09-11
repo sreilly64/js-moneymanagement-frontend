24a317032dc15965202e1418967220b3c2cbd6fb
Shane Reilly <sreilly@zipcoders-MacBook-Pro.local>
2020-09-10 20:28:21 -0400

    add login form logic

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from './../services/login/login.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  username: string = '';
+  password: string = '';
+  isUsernameValid: boolean = true;
+  error: any = null;
 
-  constructor() { }
+  constructor(
+    private loginService: LoginService,
+  ) { }
 
   ngOnInit(): void {
+    this.loginService
+      .errorSubject
+      .subscribe(errorMessage => {
+        this.error = errorMessage;
+    })
+  }
+
+  validateUsername(): void {
+    const pattern = RegExp(/^[\w-.]*$/);
+    if (pattern.test(this.username)) {
+        this.isUsernameValid = true;    
+    } else {
+        this.isUsernameValid = false;
+    }
+  }
+
+  onKey(event: any, type: string) {
+    if (type === 'username') {
+        this.username = event.target.value;
+        this.validateUsername();
+    } else if (type === 'password') {
+        this.password = event.target.value;
+    }
+  }
+
+  onSubmit(){
+    if(this.isUsernameValid){
+      this.loginService.login(this.username, this.password);
+    }
   }
 
 }
---
+++ src/app/services/login/login.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
+import { Router } from '@angular/router'
+
+const httpOption = {
+  headers: new HttpHeaders({
+    'Access-Control-Allow-Origin': '*',
+  })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+  url: any = 'http://localhost:8080/login';
+  errorSubject: any = new BehaviorSubject<any>(null);
+  errorMessage: any = this.errorSubject.asObservable();
+
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+  ) { }
+
+  login(Username: string, Password: string): any {
+    this.http.post(this.url, { Username, Password }, httpOption).toPromise().then((res: any) => {
+      if (res && res.jwt) {
+        sessionStorage.setItem('jwt', res.jwt);
+        this.errorSubject.next(null);
+        this.router.navigateByUrl('account_dashboard');
+      } else if (res.Message) {
+        this.errorSubject.next(res.Message);
+      }
+    });
+  }
+
+}
