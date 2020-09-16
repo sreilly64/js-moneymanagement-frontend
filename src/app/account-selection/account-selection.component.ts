8f700bb40b26f2a215c67bf1745504a98455f747
Julie Moon <juliemoon95@gmail.com>
2020-09-16 17:15:53 -0400

    created account selection page, added radio buttons, deposit field, and submit button

--- src/app/account-selection/account-selection.component.ts
+++ src/app/account-selection/account-selection.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account-selection.component.scss']
 })
 export class AccountSelectionComponent implements OnInit {
+  dollarInput: string = null;
+  
+  amountIsValid: boolean = false; 
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  validate(): void {
+    const dollarPattern = RegExp(/^(([1-9]\d{0,2}(,\d{3})*)|0)?\.\d{1,2}$/);
+    this.amountIsValid = dollarPattern.test(this.dollarInput);
+    console.log(this.amountIsValid);
+  }
+
+
+  onKey(event: any) {
+    this.dollarInput = event.target.value;
+    this.validate();
   }
 
 }
